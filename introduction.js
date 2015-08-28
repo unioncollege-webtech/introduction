7c794ecd89657b754f4e0e51bd5a0868b5fee667
crodeheaver <colbyrodeheaver@hotmail.com>
2015-08-28 20:09:42 +0000

    I have seen the error of my ways

--- introduction.js
+++ introduction.js
@@ -1,8 +1,8 @@
 // Your code goes here.
-var name = "Benjamin Barber";
-var age = 33;
-var major = "Computer Information Systems major";
+var name = "Colby Rodeheaver";
+var age = 24;
+var major = "Computer Information Systems";
 var classYears = ["Freshman", "Sophomore", "Junior", "Senior"];
-var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a " + classYears[3] + " " + major + " at Union College.";
+var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a " + classYears[3] + " " + major + " major at Union College.";
 
 console.log(message);
