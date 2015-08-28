6b2e6392821e3f00a10c343d11b50dece1c698ea
crodeheaver <colbyrodeheaver@hotmail.com>
2015-08-28 19:00:13 +0000

    Finished up the test

--- introduction.js
+++ introduction.js
@@ -1,1 +1,8 @@
 // Your code goes here.
+var name = "Benjamin Barber";
+var age = 33;
+var major = "Computer Information Systems major";
+var classYears = ["Freshman", "Sophomore", "Junior", "Senior"];
+var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a " + classYears[3] + " " + major + " at Union College.";
+
+console.log(message);
