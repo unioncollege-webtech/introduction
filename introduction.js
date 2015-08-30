0d9c92ed72980477c55b0d3b2cfb5492a7c39e6c
crodeheaver <colbyrodeheaver@hotmail.com>
2015-08-30 20:39:12 +0000

    But I like long lines

--- introduction.js
+++ introduction.js
@@ -3,6 +3,9 @@ var name = "Colby Rodeheaver";
 var age = 24;
 var major = "Computer Information Systems";
 var classYears = ["Freshman", "Sophomore", "Junior", "Senior"];
-var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a " + classYears[3] + " " + major + " major at Union College.";
+// But I like long lines
+var message = "Hello! My name is " + name + ". I'm " +
+              age + " years old, and I am a " + classYears[3] +
+              " " + major + " major at Union College.";
 
 console.log(message);
