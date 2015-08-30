f6ea428abd409c7abf451a762d313a960817f097
Keith Wade <keawade@gmail.com>
2015-08-30 19:07:00 +0000

    Completed.

--- introduction.js
+++ introduction.js
@@ -1,1 +1,11 @@
 // Your code goes here.
+var name = "Keith Wade";
+var age = 22;
+var major = "Computer Science";
+var classYears = ["Freshman", "Sophomore", "Junior", "Senior"];
+
+var message = "Hello! My name is " + name + ". I'm " + age +
+              " years old, and I am a " + classYears[3] + " " +
+              major + " major at Union College.";
+
+console.log(message);
