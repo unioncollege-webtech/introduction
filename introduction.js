b65a3f7bf125d5e0d4565eacab551bf88e630875
Nathan Bland <nathan.bland@gmail.com>
2015-09-03 14:32:12 -0600

    Done but... Where is the alumni option? ;)

--- introduction.js
+++ introduction.js
@@ -1,1 +1,10 @@
 // Your code goes here.
+var name = 'Nathan bland'
+var age = 23
+var major = 'C.S.'
+var classYears = ['Freshman','Sophmore','Junior','Senior']
+var message = 'Hello! My name is '+ name+
+              " I'm "+ age+ ' years old, and I am a '+
+              major+' '+ classYears[classYears.length-1]+ ' major at '+
+              'Union College.'
+console.log(message)
