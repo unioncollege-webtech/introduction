21fedcaf5d065ed5d710533f2fdf69061ee8b3ac
Nathan Bland <nathan.bland@gmail.com>
2015-09-03 15:21:41 -0600

    updated for new instructions with alumnus option

--- introduction.js
+++ introduction.js
@@ -2,9 +2,9 @@
 var name = 'Nathan bland'
 var age = 23
 var major = 'C.S.'
-var classYears = ['Freshman','Sophmore','Junior','Senior']
+var classStanding = ['Freshman','Sophmore','Junior','Senior', 'Alumnus']
 var message = 'Hello! My name is '+ name+
-              " I'm "+ age+ ' years old, and I am a '+
-              major+' '+ classYears[classYears.length-1]+ ' major at '+
+              " I'm "+ age+ ' years old, and I am a(n) '+
+              classStanding[4]+' '+ major + ' major at '+
               'Union College.'
 console.log(message)
