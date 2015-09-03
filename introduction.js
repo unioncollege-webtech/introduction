954decd7f7005dbaf4c14c531edbc7fe3ae7a04c
Nathan Bland <nathan.bland@gmail.com>
2015-09-03 16:17:09 -0600

    Fixing typos and expanding 'major' String variable.

--- introduction.js
+++ introduction.js
@@ -1,7 +1,7 @@
 // Your code goes here.
-var name = 'Nathan bland'
+var name = 'Nathan Bland'
 var age = 23
-var major = 'C.S.'
+var major = 'Computer Science'
 var classStanding = ['Freshman','Sophmore','Junior','Senior', 'Alumnus']
 var message = 'Hello! My name is '+ name+
               " I'm "+ age+ ' years old, and I am a(n) '+
