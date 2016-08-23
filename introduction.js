a8dc3d46d182a42548c44f24733a4e4bd937d17d
Keith Wade <keithwade@nanonation.net>
2016-08-23 13:04:15 -0500

    Bigger better ~~faster~~ stronger

--- introduction.js
+++ introduction.js
@@ -1,11 +1,30 @@
-// Your code goes here.
-var name = "Keith Wade";
-var age = 22;
-var major = "Computer Science";
-var classYears = ["Freshman", "Sophomore", "Junior", "Senior"];
+const name = 'Keith Wade'
+const genderFemale = false
+const age = 23
+const major = 'Computer Science'
+const school = 'Union College'
+const standings = ['freshman', 'sophomore', 'junior', 'senior', 'graduate']
+const yearsCompleted = 4
+const vowels = ['a', 'e', 'i', 'o', 'u']
 
-var message = "Hello! My name is " + name + ". I'm " + age +
-              " years old, and I am a " + classYears[3] + " " +
-              major + " major at Union College.";
+const message = `Hello, my name is ${name}. I am ${age} years old and I am ${vowels.indexOf(major.charAt(0).toLowerCase()) > -1 ? 'an' : 'a'} ${major} ${standings[yearsCompleted]} ${yearsCompleted > 3 ? 'from' : 'at'} ${school}.`
 
-console.log(message);
+console.log(message)
+
+// Fancyish things
+let wrapper = document.createElement('div')
+
+let textElement = document.createElement('h1')
+textElement.textContent = message
+
+let button = document.createElement('button', '', {})
+button.textContent = 'Click me!'
+button.onclick = function () {
+  const voice = genderFemale ? 'UK English Female' : 'UK English Male'
+  responsiveVoice.speak(message, voice)
+}
+
+wrapper.appendChild(textElement)
+wrapper.appendChild(button)
+
+document.body.appendChild(wrapper)
