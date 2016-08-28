9da13cb72fdbbe4ec4409937c31005801a09b633
bretttrana <brett.trana@gmail.com>
2016-08-28 21:45:41 +0000

    Modified to include the requirements for the assignment: Created variables and then used them in another variable called "message".

--- introduction.js
+++ introduction.js
@@ -1,1 +1,7 @@
-// Your code goes here.
+var name = "Brett Trana";
+var age = 21;
+var major = "Computer Information Systems";
+var classStanding = ["Freshman", "Sophomore", "Junior", "Senior", "Alumnus"];
+var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a " + classStanding[3] + " " + major + " major at Union College.";
+
+console.log(message);
