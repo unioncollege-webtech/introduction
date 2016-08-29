e1a60784fcc8a65b109b551fbf7f6beff65c48fd
Mark Andrew Mackinnon Eldridge <usmceldridge@gmail.com>
2016-08-29 00:51:08 +0000

    Changed a(n) to a

--- introduction.js
+++ introduction.js
@@ -4,7 +4,7 @@ var age = 27;
 var major = "Business Administration";
 var classStanding = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Alumnus'];
 
-var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a(n) " + classStanding[2] + " " + major + " major at Union College."
+var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a " + classStanding[2] + " " + major + " major at Union College."
 
 
 console.log(message)
