ced6a191b19ca09bafde54fab3a82e947928758b
anishb <bajagain.ansh@gmail.com>
2016-08-29 22:30:48 +0000

    Changed message to be displayed.

--- introduction.js
+++ introduction.js
@@ -3,8 +3,7 @@ var name = 'Anish Bajagain'
 var age = 21
 var major = 'Computer Information Systems'
 var classStanding = ['Freshman','Sophmore','Junior','Senior', 'Alumnus']
-var message = 'Hello! My name is '+ name+
-              " I'm "+ age+ ' years old, and I am a(n) '+
-              classStanding[2]+' '+ major + ' major at '+
-              'Union College.'
+var message = "Hello! My name is " + name + ". I'm " +
+              age + " years old, and I am a " + classStanding[2] +
+              " " + major + " major at Union College.";
 console.log(message);
