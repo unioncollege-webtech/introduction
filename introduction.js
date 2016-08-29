ba300aa72a119a170bb9e2dca1760c6bd314fd90
ParadoxDream <douglas.sheese@ucollege.edu>
2016-08-28 19:36:15 -0700

    First Attempt

--- introduction.js
+++ introduction.js
@@ -1,1 +1,11 @@
 // Your code goes here.
+ +var name = "Douglas Sheese";
+ +var age = 22;
+ +var major = "General Studies";
+ +var classStanding = ["Freshman", "Sophomore", "Junior", "Senior", "Alumnus"];
+ +
+ +var message = "Hello! My name is " + name + ". I'm " + age +
+ +              " years old, and I am a " + classStanding[4] + " " +
+ +              major + " major at Union College.";
+ +
+ +console.log(message);
