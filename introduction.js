446d2899081bc6cece239fe247b86de93c263281
anishb <bajagain.ansh@gmail.com>
2016-08-29 23:07:38 +0000

    Changed classStanding to show as Senior.

--- introduction.js
+++ introduction.js
@@ -4,6 +4,6 @@ var age = 21
 var major = 'Computer Information Systems'
 var classStanding = ['Freshman','Sophmore','Junior','Senior', 'Alumnus']
 var message = "Hello! My name is " + name + ". I'm " +
-              age + " years old, and I am a " + classStanding[2] +
+              age + " years old, and I am a " + classStanding[3] +
               " " + major + " major at Union College.";
 console.log(message);
