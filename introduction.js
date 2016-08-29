aa344962fee4e87fd6127be66fe2e999bf73e82d
Mark Andrew Mackinnon Eldridge <usmceldridge@gmail.com>
2016-08-29 00:47:30 +0000

    Changed age1 to age

--- introduction.js
+++ introduction.js
@@ -1,10 +1,10 @@
 // Your code goes here.
 var name = "Mark Eldridge";
-var age1 = 27;
+var age = 27;
 var major = "Business Administration";
 var classStanding = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Alumnus'];
 
-var message = "Hello! My name is " + name + ". I'm " + age1 + " years old, and I am a(n) " + classStanding[2] + " " + major + " major at Union College."
+var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a(n) " + classStanding[2] + " " + major + " major at Union College."
 
 
 console.log(message)
