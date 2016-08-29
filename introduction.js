5441a73c6090e2e3e68405c18488517a6308f315
Matilda <matildakond@gmail.com>
2016-08-29 23:05:17 +0000

    Completed and test passed.

--- introduction.js
+++ introduction.js
@@ -1,1 +1,14 @@
-// Your code goes here.
+
+var name = "Matilda Kond";
+var age = 29;
+var major = "Computer Science";
+var classStanding = [
+    "Freshman",
+    "Sophomore", 
+    "Junior", 
+    "Senior", 
+    "Alumnus",
+    ];
+var message = "Hello! My name is " + name + ". I'm " + age + " years old, and I am a " + classStanding[3] + " " + major + " major at Union College." ;
+        
+console.log(message);
