abdc20cc3057fae7a631e51cbe22de0b66d70886
anishb <bajagain.ansh@gmail.com>
2016-08-29 22:18:17 +0000

    Html file was created to grab the javascript message for Introduction.

--- introduction.js
+++ introduction.js
@@ -1,1 +1,10 @@
 // Your code goes here.
+var name = 'Anish Bajagain'
+var age = 21
+var major = 'Computer Information Systems'
+var classStanding = ['Freshman','Sophmore','Junior','Senior', 'Alumnus']
+var message = 'Hello! My name is '+ name+
+              " I'm "+ age+ ' years old, and I am a(n) '+
+              classStanding[2]+' '+ major + ' major at '+
+              'Union College.'
+console.log(message);
