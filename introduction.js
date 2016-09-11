868f28be0da82323daf9ca14571c4d545c7e7511
Ben Barber <barberboy@gmail.com>
2016-09-11 20:58:48 +0000

    Completed all steps of the assignment
    Per the assignment instructions, we logged a message that contained
    our name, age, and major.

--- introduction.js
+++ introduction.js
@@ -1,1 +1,8 @@
-// Your code goes here.
+var name = 'Benjamin Barber';
+var age = 34;
+var major = 'Computing';
+var classStanding = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Alumnus'];
+
+var message = `Hello! My name is ${name}. I'm ${age} years old, and I am an ${classStanding[4]} ${major} major at Union College.`;
+
+console.log(message)
