44c2048d47616475aa219f20620cc3fdfe2e1a6d
jeanchristian96 <tourejeanchristian@hotmail.fr>
2016-09-12 23:34:56 +0000

    FIrst introduction exercise with Javascript.

--- introduction.js
+++ introduction.js
@@ -1,1 +1,7 @@
 // Your code goes here.
+ var name = 'Jean-Christian'
+    var age = 20
+    var major = 'Computing'
+    var classstanding = ['Freshman' , 'Sophomore' , 'Junior' , 'Senior' , 'Alumnus']
+    var message = "Hello! My name is "+ name +". I'm "+ age +" years old, and I am a " + classstanding[2] +" "+ major+" major at Union College"
+    console.log(message);
